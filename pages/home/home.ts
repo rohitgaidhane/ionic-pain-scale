65f5ba9a523ba144e6096f61eee31b6fd3709151
rohitgaidhane <rohitgaidhane123@gmail.com>
2021-07-23 17:20:31 +0530

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  pain: any;
 
-  constructor(public navCtrl: NavController) {
-
+  painArray: any = [];
+  constructor(public navCtrl: NavController) {}
+  ngOnInit() {
+    this.painArray = JSON.parse(localStorage.getItem('painArray'));
   }
 
+  save() {
+    if (this.pain) {
+      debugger;
+      this.painArray.push({ pain: this.pain, time: new Date() });
+      localStorage.setItem('painArray', JSON.stringify(this.painArray));
+    } else {
+      alert();
+    }
+  }
 }
